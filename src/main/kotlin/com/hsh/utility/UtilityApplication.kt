2a28dad5d33a738e320d726eb07bb6f8a597b715
seunghun <dau4723@gmail.com>
2022-09-18 14:32:36 +0900

    init

---
+++ src/main/kotlin/com/hsh/utility/UtilityApplication.kt
@@ -0,0 +1,11 @@
+package com.hsh.utility
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UtilityApplication
+
+fun main(args: Array<String>) {
+	runApplication<UtilityApplication>(*args)
+}
---
+++ src/test/kotlin/com/hsh/utility/UtilityApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hsh.utility
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UtilityApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
